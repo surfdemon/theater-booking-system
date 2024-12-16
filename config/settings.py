c52127112c4e61e6b6d832b769e28d91bfb84ef0
Rob Beach <surfdemon@gmail.com>
2024-12-16 14:29:50 +0000

    Maint: configue database settings for local db

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com']
-
+ 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "https://*.herokuapp.com"
@@ -121,9 +121,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if os.path.exists("db.sqlite3"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
