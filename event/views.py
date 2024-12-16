4c43b3677df9dde050a51dcd9e27a46bf271e93e
[Zara Samm] <[zarasamm1996@gmail.com]>
2024-12-16 15:03:17 +0000

    Add: events  page class view and import models

--- event/views.py
+++ event/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views import View
+from django.views import generic, View
+from .models import Event, BookingTable
 
 # Create your views here.
 
@@ -7,3 +8,10 @@ from django.views import View
 class EventHome(View):
     def get(self, request):
         return render(request, 'event/home.html')
+
+
+class EventList(generic.ListView):
+    model = Event
+    template_name = "event/events.html"
+    context_object_name = "events"
+    paginate = 6
