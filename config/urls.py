4319a749b75879399205ad2fd8e16c4cfad234fe
Rob Beach <surfdemon@gmail.com>
2024-12-16 12:44:13 +0000

    Add: logo, favicon and page link names to base

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('event.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+    path('', include('event.urls')),
 ]
