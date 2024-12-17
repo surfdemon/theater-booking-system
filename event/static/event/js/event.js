3f79b425352eaa7a69bb918c8b280522c82c9c4a
Rob Beach <surfdemon@gmail.com>
2024-12-17 11:17:09 +0000

    Add: add booking success

--- event/static/event/js/event.js
+++ event/static/event/js/event.js
@@ -3,7 +3,7 @@ console.log('started event.js');
 
 // Calculate total price for tickets
 const calculateTotalPrice = (price) => {
-    const tickets = document.getElementById('tickets').value;
+    const tickets = document.getElementById('number_of_tickets').value;
     const totalPrice = tickets * parseFloat(price);
     document.getElementById('totalPrice').innerText = totalPrice;
 }
