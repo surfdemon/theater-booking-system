e20e369931bbed62686c6aa877b586e65a4ce640
Becky Williams <beckz14@gmail.com>
2024-12-18 12:40:39 +0000

    add: signup models

---
+++ about/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Signup(models.Model):
+    fname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
