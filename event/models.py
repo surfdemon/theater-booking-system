265ca2a67f75aa55ba47c370e02c5f25946d46ca
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-12-19 15:41:14 +0000

    added docstring

--- event/models.py
+++ event/models.py
@@ -14,6 +14,7 @@ class Event(models.Model):
     image = CloudinaryField('image', blank=True, null=True, default=None)
 
     def tickets_left(self):
+        '''Returns the number of tickets left for the event'''
         bookings_for_event = BookingTable.objects.filter(event=self)
         booked_tickets = bookings_for_event.aggregate(total=Sum('numberOfTickets'))['total']
         if booked_tickets is None:
