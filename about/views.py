60392188da79960ff6df944d423d7b43068c88c1
Becky Williams <beckz14@gmail.com>
2024-12-19 11:01:37 +0000

    updated: view for newsletter

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.contrib import messages
+from django.db import IntegrityError
 from .models import Signup
 
 # Create your views here.
@@ -11,6 +13,18 @@ class About(View):
         return render(request, 'about/about.html')
 
     def post(self, request):
-        email = request.POST.get('email')
-        about.save()
-        return render(request, 'about/signup_success.html')
+        email = request.POST['email']
+        name = request.POST['fname']
+
+        # if not fname or not email:
+        #     return render(request, 'about/about.html', {'error': 'Both First Name and Email are required.'})
+
+        # if Signup.objects.filter(email=email).exists():
+        #     return render(request, 'about/about.html', {'error': 'This email is already registered.'})        
+        try:
+            Signup.objects.create(fname=name, email=email).save()
+        except IntegrityError as e:
+            messages.error(self.request, "Email address already registered.")
+            return render(request, 'about/about.html')
+
+        return render(request, 'about/signup_success.html', {'fname': name})
