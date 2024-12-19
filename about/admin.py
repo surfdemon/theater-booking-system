355e13e4898d06657a4a0f66a6a8546d1ad54996
Becky Williams <beckz14@gmail.com>
2024-12-19 11:01:59 +0000

    update: admin for signup page

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Signup
 
 # Register your models here.
+
+
+@admin.register(Signup)
+class Signup(admin.ModelAdmin):
+    list_display = ['fname', 'email', 'created_at']
+    search_fields = ['fname', 'email']
+    ordering = ['-created_at']
