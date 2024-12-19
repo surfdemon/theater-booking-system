8fb76872335ad7d8d12fdf92b13499235e62168f
Rob Beach <surfdemon@gmail.com>
2024-12-19 13:21:19 +0000

    Add: image and new styling to booking and update booking page

--- event/static/event/js/event.js
+++ event/static/event/js/event.js
@@ -2,8 +2,35 @@
 console.log('started event.js');
 
 // Calculate total price for tickets
-const calculateTotalPrice = (price) => {
-    const tickets = document.getElementById('number_of_tickets').value;
+const calculateTotalPrice = (tickets, price) => {
     const totalPrice = tickets * parseFloat(price);
     document.getElementById('totalPrice').innerText = totalPrice;
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => { 
+    const updateTickets = document.getElementById('id_numberOfTickets');
+    if (updateTickets) {
+        const price = document.getElementById('price').innerText;
+        const totalPrice = updateTickets.value * parseFloat(price);
+        calculateTotalPrice(updateTickets.value, price);
+
+        updateTickets.addEventListener('change', () => {
+            const totalPrice = updateTickets.value * parseFloat(price);
+            calculateTotalPrice(updateTickets.value, price);
+        });
+    }
+
+    const newTickets = document.getElementById('number_of_tickets');
+    if(newTickets) {
+        const price = document.getElementById('price').innerText;
+        const totalPrice = newTickets.value * parseFloat(price);
+        calculateTotalPrice(newTickets.value, price); 
+
+        newTickets.addEventListener('change', () => {
+            const totalPrice = newTickets.value * parseFloat(price);
+            calculateTotalPrice(newTickets.value, price); 
+        });
+    }
+});
